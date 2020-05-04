05fcf94ec84cdd125460844f6bd0c3bd22d885a1
Philip Dexter <philip.dexter@gmail.com>
2020-05-04 13:59:39 +0300

    Simple tests

--- tests/test_cue.py
+++ tests/test_cue.py
@@ -3,5 +3,7 @@ import pytest
 
 import cue
 
-def test_test():
+def test_basic():
   assert '1' == str(cue.compile('1'))
+  assert ['1', '2', '3', '{\n\ta: 1\n}'] == [str(v) for v in cue.compile('[1,2,3,{a:1}]')]
+  assert [('a', '1'), ('b', '2')] == [(str(k), str(v)) for k, v in cue.compile('{a: 1, b: 2}')]
